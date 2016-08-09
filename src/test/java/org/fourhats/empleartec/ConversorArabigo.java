0b5aa1247e208f0ada1bb6fa69ceebcf81ad36a5
Guadalupe Huergo <guadalupe.huergo@gmail.com>
2016-08-08 21:01:21 -0300

    refactor test 90

--- src/test/java/org/fourhats/empleartec/ConversorArabigo.java
+++ src/test/java/org/fourhats/empleartec/ConversorArabigo.java
@@ -1,30 +1,29 @@
 package org.fourhats.empleartec;
-
-
+import java.util.*;
 public class ConversorArabigo {
+    private  static Map<Integer,String> simbolos;
+    static{
+        simbolos = new HashMap<Integer,String>();
+        simbolos.put(50,"L");
+        simbolos.put(40,"XL");
+        simbolos.put(10,"X");
+        simbolos.put(9,"IX");
+        simbolos.put(5,"V");
+        simbolos.put(4,"IV");
+        simbolos.put(1,"I");
+        simbolos.put(90,"XC");
+        simbolos.put(100,"C");
+    }
     public String toRoman(int numero) {
         String romano = "";
-        if(numero == 10) {
-            return  "X";
-        }
-        if(numero == 9) {
-            return  "IX";
-        }
-        if(numero == 6) {
-            return  "VI";
-        }
-        if(numero >= 5) {
-            romano +=  "V";
-            numero = numero - 5;
-        }
-        if(numero == 4) {
-            return  "IV";
-        }
-        while (numero > 0) {
-            romano += "I";
-            numero--;
+        ArrayList<Integer> numeros = new ArrayList<Integer>(simbolos.keySet());
+        Collections.sort(numeros,Collections.reverseOrder());
+        for (int i=0;i< numeros.size();i++) {
+            while(numero>= numeros.get(i)){
+                romano += simbolos.get(numeros.get(i));
+                numero = numero - numeros.get(i);
+            }
         }
         return romano;
-
     }
 }
--- src/test/java/org/fourhats/empleartec/tenis/ConversorArabigoTests.java
+++ src/test/java/org/fourhats/empleartec/tenis/ConversorArabigoTests.java
@@ -20,6 +20,31 @@ public class ConversorArabigoTests {
         chequear(8, "VIII");
         chequear(9, "IX");
         chequear(10, "X");
+        chequear(11,"XI");
+        chequear(12,"XII");
+        chequear(13,"XIII");
+        chequear(14,"XIV");
+        chequear(15,"XV");
+        chequear(16,"XVI");
+        chequear(17,"XVII");
+        chequear(18,"XVIII");
+        chequear(19,"XIX");
+        chequear(20,"XX");
+        chequear(21,"XXI");
+        chequear(22,"XXII");
+        chequear(23,"XXIII");
+        chequear(24,"XXIV");
+        chequear(25,"XXV");
+        chequear(29,"XXIX");
+        chequear(30,"XXX");
+        chequear(35,"XXXV");
+        chequear(39,"XXXIX");
+        chequear(40,"XL");
+        chequear(50,"L");
+        chequear(59,"LIX");
+        chequear(70,"LXX");
+        chequear(89,"LXXXIX");
+        chequear(90,"XC");
     }
 
     public void chequear(int numero, String esperado) {
